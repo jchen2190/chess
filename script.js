4525f760fde87e7b1053f4f4344307ae4db1695e
Jon Chen <jchen2190@gmail.com>
2023-06-20 23:33:46 -0400

    case for pawn movement

--- script.js
+++ script.js
@@ -92,6 +92,28 @@ function dragDrop(e) {
     }
 }
 
+function checkIfValid(target) {
+    const targetId = Number(target.getAttribute("square-id")) || Number(target.parentNode.getAttribute("square-id"));
+    const startId = Number(startPositionId);
+    const piece = draggedElement.id
+    console.log('targetId', targetId);
+    console.log('startId', startId);
+    console.log('piece', piece)
+
+    switch(piece) {
+        case "pawn" :
+            const starterRow = [8, 9, 10, 11, 12, 13, 14, 15]
+            if (
+                starterRow.includes(startId) && startId + width * 2 === targetId ||
+                startId + width === targetId ||
+                startId + width - 1 === targetId && document.querySelector(`[square-id="${startId + width - 1}"]`).firstChild ||
+                startId + width + 1 === targetId && document.querySelector(`[square-id="${startId + width + 1}"]`).firstChild
+                ){
+                return true;
+            }
+    }
+}
+
 function changePlayer() {
     if (playerGo === "black") {
         reverseIds();
